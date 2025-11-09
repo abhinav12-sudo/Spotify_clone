96e586fbf416775628b3d2d33ffe58cda6237643
abhinav12-sudo <abhitomar212007@gmail.com>
2025-11-09 13:08:45 +0530

    Add some javascript which we can control seekbar and play and pause button also add some css to the seekbar that is looking pretty good

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 console.log('Lets write javascript');
+let currentsong = new Audio();
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 async function getsongs(){
     let a= await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text();
@@ -15,9 +29,20 @@ async function getsongs(){
     }
     return songs
 }
+const playmusic = (track,pause = false)=>{
+    // let audio = new Audio("/songs/"+track)
+    currentsong.src = "/songs/"+track
+    if(!pause){
+        currentsong.play()
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00/00:00"
+}
 async function main(){
     //get the list of all songs
     let songs= await getsongs()
+    playmusic(songs[0],true)
     // show all the songs in the playlists
     let songul=document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for(const song of songs){
@@ -33,5 +58,39 @@ async function main(){
                                 </div>
                             </li>`;
     }
+    // attach an event listener to each song
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playmusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+    })
+    // attach an event listener to play, next and previous
+    play.addEventListener("click",()=>{
+        if(currentsong.paused){
+            currentsong.play()
+            play.src = "pause.svg"
+        }
+        else{
+            currentsong.pause()
+            play.src = "play.svg"
+        }
+    })
+    // listen for timeupdate event
+    currentsong.addEventListener("timeupdate",()=>{
+        console.log(currentsong.currentTime,currentsong.duration)
+        document.querySelector(".songtime").innerHTML=`${
+            secondsToMinutesSeconds(currentsong.currentTime)}/${
+            secondsToMinutesSeconds(currentsong.duration)}`
+        document.querySelector(".circle").style.left = (currentsong.currentTime/currentsong.duration)* 100 + "%";
+    })
+    // Add an event listener to seekbar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100
+        document.querySelector(".circle").style.left = percent + "%";
+        currentsong.currentTime = ((currentsong.duration)* percent)/100; 
+
+    })
 }
 main()
